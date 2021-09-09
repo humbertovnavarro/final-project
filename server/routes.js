634e250f3af9622fe0a0475b6593335200f89d9d
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-09-09 18:38:30 +0000

    fixed bug where streams would not delete

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,13 @@
 const ClientError = require('./client-error');
 module.exports = function routes(app, db) {
+
   app.get('/api/channel/:id', (req, res, next) => {
     const id = req.params.id;
     if (Number.parseInt(id) < 0) {
       throw new ClientError(400, 'Invalid user id');
     }
     const sql = `
-    select "userId", "userName", "isLive" from "users" left join "streams"
-    on "userId" = "channelId"
-    where "userId" = $1
-    order by "streams"."createdAt" desc limit 1;
+    select "userName" from "users" where "userId" = $1;
   `;
     const params = [id];
     db.query(sql, params)
@@ -17,21 +15,21 @@ module.exports = function routes(app, db) {
         res.json(data.rows[0]);
       }).catch(err => next(err));
   });
+
   app.get('/api/channel/:id/status', (req, res, next) => {
     const id = req.params.id;
     if (Number.parseInt(id) < 0) {
       throw new ClientError(400, 'Invalid user id');
     }
     const sql = `
-    select "isLive" from "users" left join "streams"
-    on "userId" = "channelId"
-    where "userId" = $1
-    order by "streams"."createdAt" desc limit 1;
+      select count(*) from "users" left join "streams"
+      on "userId" = "channelId"
+      where "channelId" = $1
     `;
     const params = [id];
     db.query(sql, params)
       .then(data => {
-        res.json(data.rows[0]);
+        res.json({ isLive: (data.rows[0].count > 0) });
       })
       .catch(err => next(err));
   });
