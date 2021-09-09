11be764eb9b5dd21633adf97cb06c94fbbeda712
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-09-09 18:19:22 +0000

    added new route to check for only live status of user

--- server/routes.js
+++ server/routes.js
@@ -17,4 +17,22 @@ module.exports = function routes(app, db) {
         res.json(data.rows[0]);
       }).catch(err => next(err));
   });
+  app.get('/api/channel/:id/status', (req, res, next) => {
+    const id = req.params.id;
+    if (Number.parseInt(id) < 0) {
+      throw new ClientError(400, 'Invalid user id');
+    }
+    const sql = `
+    select "isLive" from "users" left join "streams"
+    on "userId" = "channelId"
+    where "userId" = $1
+    order by "streams"."createdAt" desc limit 1;
+    `;
+    const params = [id];
+    db.query(sql, params)
+      .then(data => {
+        res.json(data.rows[0]);
+      })
+      .catch(err => next(err));
+  });
 };
