433cecc25a89898276688340c4bfbab6835b583e
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-09-10 15:55:58 -0700

    added rtmp authentication

--- server/rtmp.js
+++ server/rtmp.js
@@ -1,23 +1,42 @@
 const ClientError = require('./client-error');
+const crypto = require('crypto');
 module.exports = function rtmp(app, db) {
 
   app.post('/streams/on_publish', (req, res, next) => {
     if (req.ip !== '::ffff:127.0.0.1') {
       return;
     }
-    const { name, clientid: clientId, addr: ip } = req.body;
-    if (name.match(/^[a-zA-Z0-9_]+$/) === null) {
-      return next(new ClientError('Invalid stream name', 400));
+    const { name: channelId, clientid: clientId, addr: ip, psk: streamKey } = req.body;
+    if (!channelId || !clientId || !ip || !streamKey) {
+      res.sendStatus(404);
     }
+    if (channelId.match(/^[a-zA-Z0-9_]+$/) === null) {
+      res.sendStatus(404);
+    }
+    const hashedStreamKey = crypto.createHash('sha256').update(streamKey).digest('hex');
     const sql = `
-    insert into streams ("streamId", "channelId", "ip")
-    values ($1, $2, $3)
-  `;
-    const params = [clientId, name, ip];
-    db.query(sql, params)
-      .then(data => {
-        res.sendStatus(200);
-      }).catch(err => next(err));
+      select "userId" from "users" where "streamKey" = $1
+    `;
+    db.query(sql, [hashedStreamKey])
+      .then(result => {
+        const user = result.rows[0];
+        if (!user) {
+          res.sendStatus(404);
+        }
+        if (user.userId !== Number.parseInt(channelId)) {
+          res.sendStatus(404);
+        }
+        const sql = `
+          insert into "streams" ("channelId", "streamId", "ip")
+          values ($1, $2, $3)
+        `;
+        const params = [channelId, clientId, ip];
+        db.query(sql, params)
+          .then(() => {
+            res.sendStatus(200);
+          });
+      })
+      .catch(err => { throw new ClientError(err); });
   });
 
   app.post('/streams/on_publish_done', (req, res, next) => {
