d44a9c1f3c73353a8244891a7805c80466a78c4f
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-09-10 15:29:15 -0700

    added registration route

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,9 @@
 const ClientError = require('./client-error');
 const fs = require('fs');
 const path = require('path');
+const generateKey = require('./generate-key');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 module.exports = function routes(app, db) {
   app.get('/api/channel/:id', (req, res, next) => {
     const id = Number.parseInt(req.params.id);
@@ -41,7 +44,6 @@ module.exports = function routes(app, db) {
       ).catch(err => next(err));
   });
 
-
   app.post('/api/channels/query', (req, res, next) => {
     const offset = Number.parseInt(req.params.offset) || 1;
     if (offset <= 0 || Number.isNaN(offset)) {
@@ -60,5 +62,44 @@ module.exports = function routes(app, db) {
         res.json(data.rows);
       }
       ).catch(err => { console.error(err); });
+  });
+
+  app.post('/api/register', (req, res, next) => {
+    if (!req.body.userName || !req.body.password || !req.body.email) {
+      throw new ClientError(400, 'Missing username or password');
+    }
+    const invalidUserName = /[^a-zA-Z0-9_]/.test(req.body.userName);
+    if (invalidUserName) {
+      throw new ClientError(400, 'Invalid username');
+    }
+    const { userName, password, email } = req.body;
+    argon2.hash(password)
+      .then(hash => {
+        const streamKey = generateKey();
+        const sql = `
+          select "userName" from "users" where LOWER("userName") = $1
+        `;
+        const params = [userName.toLowerCase()];
+        db.query(sql, params)
+          .then(data => {
+            if (data.rows.length > 0) {
+              throw new ClientError(400, 'Username already exists');
+            }
+            const sql = `
+            insert into "users" ("userName", "hash", "email", "streamKey")
+            values ($1, $2, $3, $4)
+            returning "userId", "userName";
+            `;
+            const params = [userName, hash, email, streamKey.hash];
+            db.query(sql, params)
+              .then(data => {
+                const payload = data.rows[0];
+                payload.streamKey = streamKey.key;
+                payload.jwt = jwt.sign(payload.userId, process.env.TOKEN_SECRET);
+                res.json(payload);
+              }).catch(err => next(err));
+          }).catch(err => next(err));
+      })
+      .catch(err => next(err));
   });
 };
