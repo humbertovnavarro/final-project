3d0973efcee400a368539799935b657ab956ceee
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-09-27 17:32:53 +0000

    image update fix

--- client/components/avatar.jsx
+++ client/components/avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 class Avatar extends React.Component {
   render() {
     const source = `/avatar/${this.props.channelId}`;
-    return <div className={`avatar ${this.props.isLive ? 'live' : ''}?time=${this.props.time}`}>
+    return <div className={`avatar ${this.props.isLive ? 'live' : ''}?time=${this.props.time || Date.now()}`}>
       <img src={source} alt="Avatar"/>
     </div>;
   }
