999b777700a10b61f7798362f95fa84e175ce516
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-09-28 22:06:12 +0000

    fixed blank space, fixed query of empty string bug, added css to fix scrolling issue

--- client/components/avatar.jsx
+++ client/components/avatar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import AppContext from '../app-context';
 class Avatar extends React.Component {
+  static contextType = AppContext;
   render() {
-    const source = `/avatar/${this.props.channelId}`;
-    return <div className={`avatar ${this.props.isLive ? 'live' : ''}?time=${this.props.time || Date.now()}`}>
+    const source = `/avatar/${this.props.channelId}?time=${this.context.time}`;
+    return <div className={`avatar ${this.props.isLive ? 'live' : ''}`}>
       <img src={source} alt="Avatar"/>
     </div>;
   }
